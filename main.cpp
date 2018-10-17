833d88562847620d4f317e3062c802fa726662c0
Jakub Sobolewski <jakupsob@gmail.com>
2018-10-17 17:53:05 +0200

    Added scripts to animate data in gnuplot

--- main.cpp
+++ main.cpp
@@ -5,19 +5,12 @@
 int main()
 {
 	GasSimulation::Gas<double> gas;
-	double a = gas.a;
-	Utils::Vector<double> b0(a,0,0);
-	Utils::Vector<double> b1(a/2,a*sqrt(3)/2,0);
-	Utils::Vector<double> b2(a/2,a*sqrt(3)/6,a*sqrt(2.f/3));
-	Utils::BaseVector<double> base(b0,b1,b2);
+	Utils::BaseVector<double> base(Utils::Vector<double>(gas.a,0,0),Utils::Vector<double>(gas.a/2,gas.a*sqrt(3)/2,0),Utils::Vector<double>(gas.a/2,gas.a*sqrt(3)/6,gas.a*sqrt(2.f/3)));
 	gas.base = base;	
-
 	gas.CalculateInitialPositions();
 	gas.CalculateInitialMomentum(); 
 	gas.CalculatePotentialAndForces();
 	gas.FlushToFiles();
-
-
 	gas.Simulation();
 	return 1;
 }
