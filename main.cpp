a6a7fdd42395fe106562ad957a8842b869008a2b
Jakub Sobolewski <jakupsob@gmail.com>
2018-11-05 18:16:16 +0100

    Minor changes

--- main.cpp
+++ main.cpp
@@ -5,20 +5,19 @@
 int main()
 {
 	GasSimulation::Gas<double> gas;
-	gas.SetParams(5, 100., 0.38, 2.3);
+	gas.SetParams(5, 1000., 0.38, 2.3);
 	gas.CalculateInitialPositions();
 	gas.CalculateInitialMomentum(); 
 	gas.CalculatePotentialAndForces();
 	gas.FlushToFiles();	
-	// gas.Simulation();
 
-	// gas.SetParams(3, 1000., 0.38, 1.2);
-	// gas.StabilityTest(std::pow(10,-6), 5*std::pow(10,-3), 100, 1.);
+	gas.Simulation(0.001, 100);
+
+	gas.SetParams(3, 1000., 0.38, 1.2);
+	gas.StabilityTest(std::pow(10,-6), 5*std::pow(10,-3), 100, 1.);
 	
-	// gas.SetParams(5, 1000., 0.38, 2.3);
-	// gas.CrystalConstantTest(0.35, 0.40, 1000);
-
-
+	gas.SetParams(5, 500., 0.38, 2.3);
+	gas.CrystalConstantOptimization(0.35, 0.40, 1000);
 
 	return 1;
 }
